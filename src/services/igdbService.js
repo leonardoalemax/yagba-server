0e89d82978d7d6d0b906d8adc7faeb796c4d5a19
Leonardo Ferreira <leualemax@gmail.com>
2021-01-29 13:45:29 -0300

    add detail route

--- src/services/igdbService.js
+++ src/services/igdbService.js
@@ -1,19 +1,68 @@
 import axios from 'axios'
 
 const igdbService = (cs, app) => {
+
+  const headers = {
+    'Accept': 'application/json',
+    'Client-ID': 'kn6ug0oxr3f0ifqp0jmz2lkda3m77u',
+    'Authorization': 'Bearer 4lzb0b0th1bdwm6sxt8jh3ty78lw45',
+  };
+
   app.get('/igdb/search', async (req, res) => {
     cs.log(`search igdb for term ${req.query.term}`)
+
+    const fields = [
+      "name",
+      "cover.url",
+      "platforms.name",
+      "platforms.platform_family.name",
+      "release_dates.human"
+    ]
 
     try {
       const { data } = await axios({
         url: "https://api.igdb.com/v4/games",
         method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Client-ID': 'kn6ug0oxr3f0ifqp0jmz2lkda3m77u',
-          'Authorization': 'Bearer 4lzb0b0th1bdwm6sxt8jh3ty78lw45',
-        },
-        data: `fields name, cover.url, platforms.name, platforms.platform_family.name, release_dates.human; search "${req.query.term}";`
+        headers: headers,
+        data: `fields ${fields.join(", ")}; where id = ${req.query.id};`
+      })
+      res.send(data)
+    } catch (error) {
+      cs.log(error)
+    }
+  });
+
+  app.get('/igdb/detail', async (req, res) => {
+    cs.log(`search igdb for term ${req.query.term}`)
+
+    const fields = [
+      "name",
+      "cover.url",
+      "artworks.url",
+      "alternative_names.name",
+      "game_engines.name",
+      "franchises.name",
+      "platforms.name",
+      "platforms.platform_family.name",
+      "release_dates.human",
+      "game_modes.name",
+      "genres.name",
+      "screenshots.url",
+      "slug",
+      "standalone_expansions.name",
+      "storyline",
+      "external_games.name",
+      "summary",
+      "themes",
+      "parent_game.name"
+    ]
+
+    try {
+      const { data } = await axios({
+        url: "https://api.igdb.com/v4/games",
+        method: 'POST',
+        headers: headers,
+        data: `fields ${fields.join(", ")}; where id = ${req.query.id};`
       })
       res.send(data)
     } catch (error) {
