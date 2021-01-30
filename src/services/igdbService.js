077c1a5a0764a56006b7a5622d3ae4a32424749b
Leonardo Ferreira <leualemax@gmail.com>
2021-01-30 01:15:51 -0300

    change route to games and accept query as a param

--- src/services/igdbService.js
+++ src/services/igdbService.js
@@ -8,61 +8,15 @@ const igdbService = (cs, app) => {
     'Authorization': 'Bearer 4lzb0b0th1bdwm6sxt8jh3ty78lw45',
   };
 
-  app.get('/igdb/search', async (req, res) => {
-    cs.log(`search igdb for term ${req.query.term}`)
-
-    const fields = [
-      "name",
-      "cover.url",
-      "platforms.name",
-      "platforms.platform_family.name",
-      "release_dates.human"
-    ]
+  app.get('/igdb/games', async (req, res) => {
+    cs.log(`querying igdb with ${req.query.query}`)
 
     try {
       const { data } = await axios({
         url: "https://api.igdb.com/v4/games",
         method: 'POST',
         headers: headers,
-        data: `fields ${fields.join(", ")}; where id = ${req.query.id};`
-      })
-      res.send(data)
-    } catch (error) {
-      cs.log(error)
-    }
-  });
-
-  app.get('/igdb/detail', async (req, res) => {
-    cs.log(`search igdb for term ${req.query.term}`)
-
-    const fields = [
-      "name",
-      "cover.url",
-      "artworks.url",
-      "alternative_names.name",
-      "game_engines.name",
-      "franchises.name",
-      "platforms.name",
-      "platforms.platform_family.name",
-      "release_dates.human",
-      "game_modes.name",
-      "genres.name",
-      "screenshots.url",
-      "slug",
-      "standalone_expansions.name",
-      "storyline",
-      "external_games.name",
-      "summary",
-      "themes",
-      "parent_game.name"
-    ]
-
-    try {
-      const { data } = await axios({
-        url: "https://api.igdb.com/v4/games",
-        method: 'POST',
-        headers: headers,
-        data: `fields ${fields.join(", ")}; where id = ${req.query.id};`
+        data: req.query.query
       })
       res.send(data)
     } catch (error) {
